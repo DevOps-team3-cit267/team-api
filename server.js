d1abe8f9aba29a5f5d8fcd8ce7d97d102f735bab
mckayythomas <mckayycollins@gmail.com>
2024-02-24 14:16:02 -0500

    add mckay route

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const express =require('express');
+const express = require("express");
 
 const port = 3000;
 
 const app = express();
 
-app.get('/', (req,res) => {res.send("Hello Browser")});
+app.get("/", (req, res) => {
+  res.send("Hello Browser");
+});
 
-app.get('/brent', (req,res) => {res.send("Hello Brent")});
+app.get("/brent", (req, res) => {
+  res.send("Hello Brent");
+});
 
-app.listen(port, ()=>console.log("Listening"));
+app.get("/mckay", (req, res) => {
+  res.send("Hello McKay");
+});
+
+app.listen(port, () => console.log("Listening"));
