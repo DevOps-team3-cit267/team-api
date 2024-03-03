83089ceff8b751d4e633d3cd82ce9c52133c65d1
brrennnnt <brentroth15@gmail.com>
2024-03-02 20:29:57 -0700

    fix

--- test/brent.test.js
+++ test/brent.test.js
@@ -1,4 +1,4 @@
-test('Check if Max is returned from API', async () => {
+test('Check if Brent is returned from API', async () => {
     const response = await fetch('http://localhost:3000/brent');
     const name = await response.text();
     expect(name).toBe("brent");
